db2825244ada6bd8481717cdd9fa143ba65d1209
Dannyson <DannysonK@hotmail.com>
2026-07-16 19:10:53 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,20 @@
+// Classe que representa um personagem do jogo, com suas características básicas
 public class Personagem {
+
+    // Atributos que armazenam as características do personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
-
-    }
-
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+    // Método responsável por exibir as informações do personagem de forma legível
+    void exibirStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
+        System.out.println("-----------------------------");
     }
 }
-

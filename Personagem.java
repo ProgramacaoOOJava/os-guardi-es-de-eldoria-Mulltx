c9504abcfa47abebc14b31207785b9ca9cf746a3
Dannyson <DannysonK@hotmail.com>
2026-07-20 21:26:33 -0300

    Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,20 +1,31 @@
-// Classe que representa um personagem do jogo, com suas características básicas
-public class Personagem {
+// Superclasse abstrata que define as características e o comportamento comum a todo personagem
+public abstract class Personagem {
 
-    // Atributos que armazenam as características do personagem
+    // Atributos comuns a qualquer tipo de personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    // Método responsável por exibir as informações do personagem de forma legível
-    void exibirStatus() {
+    // Construtor que inicializa os dados básicos do personagem
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
+    }
+
+    // Método concreto reaproveitado por todas as subclasses, exibe os dados comuns do personagem
+    public void exibirStatus() {
         System.out.println("Nome: " + nome);
         System.out.println("Classe: " + classe);
         System.out.println("Nível: " + nivel);
         System.out.println("Pontos de Vida: " + pontosDeVida);
         System.out.println("Poder Base: " + poderBase);
-        System.out.println("-----------------------------");
     }
+
+    // Método abstrato: cada subclasse deve implementar sua própria habilidade especial
+    public abstract void usarHabilidadeEspecial();
 }

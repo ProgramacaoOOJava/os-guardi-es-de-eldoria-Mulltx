b8f3dd381f930543c26676e42f13b693cd6bcaf5
Dannyson <DannysonK@hotmail.com>
2026-07-22 19:32:41 -0300

    Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -2,7 +2,7 @@
 public class Guerreiro extends Personagem {
 
     // Atributo próprio do Guerreiro: a arma que ele utiliza
-    String arma;
+    private String arma;
 
     // Construtor que usa super() para inicializar os dados herdados da superclasse
     public Guerreiro(String nome, int nivel, int pontosDeVida, double poderBase, String arma) {
@@ -10,9 +10,13 @@ public class Guerreiro extends Personagem {
         this.arma = arma;
     }
 
+    public String getArma() {
+        return arma;
+    }
+
     // Sobrescrita do método abstrato com o comportamento específico do Guerreiro
     @Override
     public void usarHabilidadeEspecial() {
-        System.out.println("Guerreiro " + nome + " ataca com " + arma + "!");
+        System.out.println("Guerreiro " + getNome() + " ataca com " + arma + "!");
     }
 }
